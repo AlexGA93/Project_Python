3cfad1d95cd010dbc6b2b4025f6425809f69644e
AlexGA93 <alexgimat@hotmail.com>
2017-10-19 12:50:48 +0200

    He quitado comentarios innecesarios. He borrado una funcion y su posterior readaptacion en el menu He probado a meter un metodo que detecte que solo funcionara en Windows(puede que algun dia lo haga en otros) Edicion de formato en los inputs para nombres de fichero Corrección de errores a la hora de introducir una opcion numerica
    comentarios para enlazar varios escripts(en prueba)

--- CUERPO.py
+++ CUERPO.py
@@ -3,6 +3,8 @@
 #guardamos los txt en carpetas llamadas por la comunidad( donde se guardaran varios txt de colegios)---> HACER MENU AL INICIAR PARA ACCEDER A LAS CARPETAS
 #interfaz grafica
 #cifrdo por pass--> comparacion con txt---> si es erronea se enviara un codigo
+#from ARCHIVO import FUNCION
+#archivo.funcion(args)
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 import os
 import shutil
