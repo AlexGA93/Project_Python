4e227b350aa24ff1e7d526d6aa1be93a04bf917b
alejandro g <alexgimat@hotmail.com>
2017-10-22 17:45:50 +0200

    gui en carpetas pruebas

--- pruebas/GUI.py
+++ pruebas/GUI.py
@@ -1,3 +1,9 @@
+'''
+https://www.tutorialspoint.com/python/python_gui_programming.htm
+
+https://www.tutorialspoint.com/python/tk_button.htm
+'''
+
 from tkinter import *
 from tkinter import ttk
 raiz=Tk()
